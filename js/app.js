3b7ce21a7069614c3a2c24fe4197633fbb6a0018
Esdras Ceballos Zurita <esdras.ceballosz@outlook.com>
2023-12-21 11:49:30 -0600

    app.js: error type messages added in form validation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 // Variables
 
-const expenseAddForm = document.querySelector('form#agregar-gasto');
+const expenseForm = document.querySelector('form#agregar-gasto');
 const expenseList = document.querySelector('#gastos ul'); 
+const primaryContent = document.querySelector('div.contenido.primario');
 
 
 // Events
 
 function eventListeners (){
     document.addEventListener('DOMContentLoaded', askBudget )
+    expenseForm.addEventListener('submit', addExpense);
 };
 
 eventListeners();
@@ -23,6 +25,31 @@ class UI {
 
        document.querySelector('#total').textContent = budget;
        document.querySelector('#restante').textContent = remaining;
+    }
+
+    alertMessage(message, type){
+
+        const containerMessage = document.createElement('div');
+        containerMessage.classList.add('text-center', 'alert');
+
+        //validating type message 
+
+        if (type == 'error'){
+            containerMessage.classList.add('alert-danger');
+        }else{
+            containerMessage.classList.add('alert-success');
+        };
+
+        // adding alert message
+        containerMessage.textContent = message;
+
+        //insert alert message HTML
+        primaryContent.insertBefore(containerMessage, expenseForm);
+
+        //delete alert message
+        setTimeout( () =>{
+            containerMessage.remove();
+        }, 3000 );
     }
 }
 
@@ -55,4 +82,31 @@ function askBudget (){
 
     // insert HTML of the Budget
     userInterface.insertBudget(budget);
+};
+
+
+function addExpense (event){
+    event.preventDefault();
+
+    const expenseName = document.querySelector('input#gasto').value;
+    const amountExpense = document.querySelector('input#cantidad').value; 
+
+
+    // Form validation 
+
+    if ( expenseName === "" || amountExpense === "" ){
+
+        userInterface.alertMessage('Ambos campos son obligatorios','error');
+
+        return;
+
+    } else if( amountExpense <= 0 || isNaN(amountExpense) ){
+
+        userInterface.alertMessage('Cantidad no válida','error');
+
+        return;
+
+    };
+
+    console.log("Agregando gasto...");
 };
